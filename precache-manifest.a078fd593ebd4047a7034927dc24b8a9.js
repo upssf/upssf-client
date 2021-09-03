3c8ded4780de85bbeb97d2211384d2f365f572e9
Orlando Carnate <orlando.carnate@gmail.com>
2021-09-02 23:51:07 -0500

    Updates

---
+++ precache-manifest.a078fd593ebd4047a7034927dc24b8a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cce40a74be477510f27aceac3017b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ec7f4e25cd6de7f9cff",
+    "url": "/static/css/main.414ae15a.chunk.css"
+  },
+  {
+    "revision": "c10ccdd9a1c7702daf0a",
+    "url": "/static/js/2.95c6b1d7.chunk.js"
+  },
+  {
+    "revision": "8ec7f4e25cd6de7f9cff",
+    "url": "/static/js/main.52c7bc01.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b0ca977cd5959ec74af163ddb7945745",
+    "url": "/static/media/upssf-logo.b0ca977c.svg"
+  }
+]);
