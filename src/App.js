f0c14dbff4f8842f8b5e3009726f628edcc53e2d
Orlando Carnate <orlando.carnate@gmail.com>
2023-09-23 19:42:53 -0500

    use HashRouter for Git Pages

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
-// import { IntlProvider, addLocaleData } from "react-intl";
 import Home from "./pages/Home";
 import Mission from "./pages/Mission";
 import Officers from "./pages/Officers";
 import Scholars from "./pages/Scholars";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <div>
+    <div>
+      <HashRouter>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/Home" element={Home} /> */}
-          <Route path="/Mission" element={<Mission />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Mission" element={<Mission />} />
           <Route path="/Officers" element={<Officers />} />
           <Route path="/Scholars" element={<Scholars />} />
         </Routes>
+
         <Footer />
-      </div>
-    </Router>
+      </HashRouter>
+    </div>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,42 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 function NavBar() {
     return (
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-            <div className="container">
-                <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/Mission">Our Mission</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/Officers">Officers</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/Scholars">Our Scholars</a>
-                        </li>
-
-                    </ul>
-                </div>
-                <div className="mx-auto order-0">
-                    <a className="navbar-brand mx-auto" href="/"><h2>U.P. Silak Silab Foundation</h2></a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                    <ul className="navbar-nav ml-auto">
-                        {/* <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Blog (Coming Soon)</a>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <div className="container">
+          <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/Mission">
+                  Our Mission
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Officers">
+                  Officers
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Scholars">
+                  Our Scholars
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="mx-auto order-0">
+            <Link className="navbar-brand mx-auto" to="/">
+              <h2>U.P. Silak Silab Foundation</h2>
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target=".dual-collapse2"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+          <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul className="navbar-nav ml-auto">
+              {/* <li className="nav-item">
+                            <a className="nav-link disabled" to="#">Blog (Coming Soon)</a>
                         </li> */}
-                        <li className="nav-item">
-                            <a className="nav-link" href="mailto:upssfchicago@gmail.com">upssfchicago@gmail.com</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+              <li className="nav-item">
+                <Link className="nav-link" to="mailto:upssfchicago@gmail.com">
+                  upssfchicago@gmail.com
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
 }
 
 export default NavBar;
